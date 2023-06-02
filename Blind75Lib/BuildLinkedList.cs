982cef7409c1a2dcbbb3532b857dfaef8303bf58
Jaques Strydom <jaques.strydom@gmail.com>
2023-06-02 12:24:56 +0200

    Added LeetCode question 141. Linked List Cycle
    Added new method to library to build a list that contains a loop

--- Blind75Lib/BuildLinkedList.cs
+++ Blind75Lib/BuildLinkedList.cs
@@ -18,12 +18,31 @@ namespace Blind75Lib
             return val.next;
         }
 
+        public static ListNode BuildList(List<int> list, int loopBackPos)
+        {
+            ListNode tmp = new ListNode(null);
+            ListNode dummy = tmp;
+            ListNode? loopPos = null;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                tmp.next = new ListNode(list[i]);
+                tmp = tmp.next;
+                if (i == loopBackPos)
+                    loopPos = tmp;
+            }
+
+            if (loopBackPos >= 0) tmp.next = loopPos;
+
+            return dummy.next;
+        }
+
         public static int[] ConvertToArray(ListNode list)
         {
             List<int> result = new();
             while (list != null)
             {
-                result.Add(list.val);
+                result.Add((int)list.val);
                 list = list.next;
             }
 
